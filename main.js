8cf34941c6bceebe83a7eb7df97c6c0e4138f3af
oonqt <lcupples2187@gmail.com>
2024-03-26 01:01:36 -0700

    Episode/movie to torrent matching logic

--- main.js
+++ main.js
@@ -1,17 +1,71 @@
 const { qBittorrentClient } = require('@robertklep/qbittorrent');
-const { Radarr } = require('@jc21/radarr-api');
-const { Sonarr } = require('@jc21/sonarr-api');
-
-const log = msg => console.log(`${new Date().toISOString()}: ${msg}`);
-
+const axios = require('axios');
 const qbit = new qBittorrentClient(process.env.QBIT_URL, process.env.QBIT_USER, process.env.QBIT_PASS);
-const sonarr = new Sonarr(process.env.SONARR_URL, process.env.SONARR_API_KEY);
-const radarr = new Radarr(process.env.RADARR_URL, process.env.RADARR_API_KEY);
 
 const check = async () => {
     log('Checking...');
+
+    let torrents = await qbit.torrents.info();
+    let movies = await radarrRequest('queue/details');
+    let episodes = await sonarrRequest('queue/details');
+
+    for (episode of episodes) {
+        let torrent = torrents.filter(torrent => {
+            return episode.downloadId.toLowerCase() === torrent.hash;
+        });
+
+        if (torrent) {
+
+        }
+    }
+
+    for (movie of movies) {
+        let torrent = torrents.filter(torrent => {
+            return movie.downloadId.toLowerCase() === torrent.hash;
+        });
+
+        if (torrent) {
+            console.log(torrent);    
+        }
+    }
+}
+
+const log = msg => console.log(`${new Date().toISOString()}: ${msg}`);
+
+const radarrRequest = async (route, method, query, body) => {
+    const url = new URL(`${process.env.RADARR_URL}/api/v3/${route}`);
+
+    if (query) query.forEach(param => url.searchParams.append(param.name, param.value));
+
+    url.searchParams.append('apikey', process.env.RADARR_API_KEY);
+
+    let res = await axios({
+        url,
+        method: method || 'GET',
+        body: body || null
+    });
+
+    return res.data;
+}
+
+const sonarrRequest = async (route, method, query, body) => {
+    const url = new URL(`${process.env.SONARR_URL}/api/v3/${route}`);
+
+    if (query) query.forEach(param => url.searchParams.append(param.name, param.value));
+
+    url.searchParams.append('apikey', process.env.SONARR_API_KEY);
+
+    let res = await axios({
+        url,
+        method: method || 'GET',
+        body: body || null
+    });
+
+    return res.data;
 }
 
 log('Starting application...');
 
 setInterval(check, process.env.CHECK_INTERVAL_SECONDS * 1000);
+
+check();
